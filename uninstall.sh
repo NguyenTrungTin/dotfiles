5d37e6ef00f72666ecbbc4e747f88925580326f8
Nguyen Trung Tin <trungtinemail@gmail.com>
2018-11-22 18:26:24 +0700

    :zap: Update install.sh

--- uninstall.sh
+++ uninstall.sh
@@ -10,12 +10,14 @@ DOTDIR='.vim/ .tmux/ .hyper_plugins/ .oh-my-zsh/'
 # Uninstall dotfiles
 echo 'Uninstalling dotfiles...'
 rm -rf $DOTFILES $DOTDIR 2>/dev/null
+rm -rf ~/.dotfiles
 
 # Revert old dotfiles
 echo 'Reverting old dotfiles...'
 cd ~/.dotfiles-backup
 mv $DOTFILES $DOTDIR ~/ 2>/dev/null
 cd ~
+rm -rf ~/.dotfiles-backup
 
 # Done!
 echo 'Done!'
