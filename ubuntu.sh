9dcefaeb658562d5ba2844648c6d2c9340af356f
Nguyen Trung Tin <tin@tintrungnguyen.com>
2019-02-25 14:58:35 +0700

    Automatically install docker and netstat

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+
 # Install common dependencies
-sudo apt -y install software-properties-common build-essential checkinstall wget curl git libssl-dev apt-transport-https ca-certificates
+sudo apt -y install software-properties-common build-essential checkinstall wget curl git libssl-dev apt-transport-https ca-certificates gnupg-agent
+
+# Install Docker and docker-compose
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install net-tools, includes aditional utilisties such as arp, ifconfig, netstat, rarp, nameif and route
+sudo apt install net-tools
 
 # Install EditorConfig
 sudo apt-get install editorconfig
@@ -11,7 +27,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 nvm install --lts
 
 # Install Yarn
-sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+sudo apt-get install --no-install-recommends yarn
 
 # Install eslint
 sudo npm install -g eslint
