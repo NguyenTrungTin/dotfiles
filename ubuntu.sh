47eb893b86697a55c86289d69f52b9e0217f68f2
Nguyen Trung Tin <tin@tintrungnguyen.com>
2019-04-15 14:22:01 +0700

    Add git-extras and alacritty config

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,6 +16,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Install git-extras
+sudo apt-get install git-extras
+
 # Install net-tools, includes aditional utilisties such as arp, ifconfig, netstat, rarp, nameif and route
 sudo apt install net-tools
 
